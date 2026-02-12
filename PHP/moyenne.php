81201c31d25ab6cb7ad8aa395a74955ce5e0a8f5
BEN <benoitnikiema042@>
2026-02-12 23:02:17 +0000

    Initial commit: Gestion Etudiants PHP

---
+++ PHP/moyenne.php
@@ -0,0 +1,37 @@
+<?php
+$Etudiants = [
+    ["NIKIEMA", "BENOIT", 10],
+    ["ZONGO", "MICHEL", 14],
+    ["NARE", "FRANCK", 16],
+    ["SALIM", "ODG", 18]
+];
+
+$Sommenote = 0;
+$NombreEtudiants = count($Etudiants);
+
+foreach ($Etudiants as $Etudiant) {
+    $prenom = $Etudiant[0];
+    $nom = $Etudiant[1];
+    $note = $Etudiant[2];
+
+    echo $prenom . " " . $nom . " : " . $note . "</br>";
+    
+    if ($note >= 16) {
+        echo "Mention Tres bien</br>";
+    } elseif ($note >= 14) {
+        echo "Mention Bien</br>";
+    } elseif ($note >= 10) {
+        echo "Mention Passable</br>";
+    } else {
+        echo "Insuffisant</br>";
+    }
+    
+    $Sommenote += $note;
+    echo "</br>";
+}
+
+if ($NombreEtudiants > 0) {
+    $Moyenneclasse = $Sommenote / $NombreEtudiants;
+    echo "La moyenne generale de la classe est : " . $Moyenneclasse . "</br>";
+}
+
